a667a48fb1033fad8e8aa826afa2e5369667b72e
Flavio Duarte <flaviogabriel@gmail.com>
2017-06-19 22:48:31 -0300

    dicionário com dados organizado por livro/capítulo

--- bible/spiders/spider.py
+++ bible/spiders/spider.py
@@ -5,10 +5,16 @@ from bible.items import Verse
 
 class BibleSpider(scrapy.Spider):
     # NVI: 129 ARA: 1608
+
     name = 'bible'
-    start_urls = [
-        'https://events.bible.com/api/bible/chapter/3.1?id=1608&reference=GEN.1'
-    ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.base_url = 'https://events.bible.com/api/bible/chapter/3.1?id=1608&reference='
+        self.start_urls = [
+            f'{self.base_url}GEN.1'
+        ]
 
     def parse(self, response):
         data = json.loads(response.body)
@@ -21,19 +27,22 @@ class BibleSpider(scrapy.Spider):
         )
 
         verses = {}
-        book, chapter = data['reference']['usfm'][0].split(".")
+        book = data['reference']['human'].split(" ")[0]
+        chapter = data['reference']['usfm'][0].split(".")[1]
 
         for verse in html.css(".verse"):
             number = verse.xpath('@data-usfm').extract_first().split(".")[-1]
             text = verse.css('.content::text').extract()
 
-            if number not in verses:
-                verses[number] = Verse(
-                    book=book,
-                    chapter=chapter,
-                    verse=number,
-                    text=''
-                )
-            verses[number]['text'] += ''.join(text).strip()
+            if book not in verses:
+                verses[book] = {}
+
+            if chapter not in verses[book]:
+                verses[book][chapter] = {}
+
+            if number not in verses[book][chapter]:
+                verses[book][chapter][number] = ''
+
+            verses[book][chapter][number] += ''.join(text).strip()
 
         yield verses
